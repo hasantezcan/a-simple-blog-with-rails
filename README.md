b97da4a56e736a24312d47b098de02e7c1dc8370
hasantezcan <hasantezcan77@gmail.com>
2018-10-08 01:51:45 +0300

    :tada: init

---
+++ README.md
@@ -0,0 +1,1 @@
+[Getting Started with Rails](https://guides.rubyonrails.org/getting_started.html)
---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
